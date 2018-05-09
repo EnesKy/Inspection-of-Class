6319ad37c110d2e8acbd94b643fdf22e4eb10c6b
Enes Kamil YILMAZ <eneskamil.yilmaz@stu.fsm.edu.tr>
2018-05-09 17:24:37 +0300

    Added a java project that reads value from RFID-RC522
    To use the project, you need to add the pi4j-core.jar file as a library.

---
+++ readingFromRC522/readingFromRC522/src/readingFromRC522/ReadRFID.java
@@ -0,0 +1,26 @@
+package readingFromRC522;
+
+/**
+ * Created by Liang on 2016/3/7.
+ */
+public class ReadRFID {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while (true) {
+
+            RaspRC522 rc522 = new RaspRC522();
+            String strUID;
+            byte tagid[] = new byte[5];
+
+            rc522.Select_MirareOne(tagid);
+            strUID = Convert.bytesToHex(tagid);
+            if (!strUID.toString().equals("0000000000")) {
+                System.out.println("Kart okunuyor...");
+                System.out.println("Kart değeri : " + strUID.toString());
+                Thread.sleep(500);
+            }
+
+        }
+    }
+}
