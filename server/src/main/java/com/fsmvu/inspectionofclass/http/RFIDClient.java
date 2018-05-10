c359ac470931b2701206779100b786b077cd2ee2
Bilal Ekrem Harmansa <bilalekremharmansa@hotmail.com>
2018-05-10 13:45:00 +0300

    rfid client implemented

---
+++ server/src/main/java/com/fsmvu/inspectionofclass/http/RFIDClient.java
@@ -0,0 +1,45 @@
+package com.fsmvu.inspectionofclass.http;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class RFIDClient {
+    public static String BASE_URL = "localhost://";
+
+    private RFIDClient () {
+    
+    }
+
+    /** returns  */
+    static int request(String courseCode, String cardNo) {
+        HttpURLConnection connection = null;
+        try{
+            String params = String.format("UTF-8", 
+                            "inspect?course=%s&card=%s", courseCode, cardNo);
+
+            URL url = new URL(BASE_URL + params);
+            connection = (HttpURLConnection) url.openConnection();  
+            connection.setRequestMethod("GET");
+
+            InputStream in = connection.getInputStream();
+            byte[] rawData = new byte[in.available()];
+            in.read(rawData);
+            /** if responds needs */
+            String respond = new String(rawData);
+            
+            return connection.getResponseCode();
+        } catch(MalformedURLException ex) {
+            ex.printStackTrace();
+            return 400;
+        } catch(IOException ex) {
+            ex.printStackTrace();
+            return 400;
+        } finally {
+            if(connection!=null) connection.disconnect();
+        }
+    }
+
+}
